16c0bd29b7b48d172d526bad1f34a4d527450452
tungpham <tungpham@lecle.co.kr>
2023-04-18 10:51:00 +0700

    correct biconomy 2.0.35&below initialization

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let {Biconomy} = require("@biconomy/mexa");
+let { Biconomy } = require("@biconomy/mexa");
 let Web3 = require("web3");
 require("dotenv").config();
 
@@ -11,37 +11,30 @@ const newQuote = "testttt123";
 
 async function main() {
     const customProvider = new Web3.providers.HttpProvider('https://matic-mumbai.chainstacklabs.com');
-    const web3 = new Web3(customProvider);
-    // Initialize constants
-    let contract = new web3.eth.Contract(
-        contractAbi,
-        contractAddr
+    const biconomyWithWeb3 = new Biconomy(
+        customProvider,
+        {apiKey: "kAsdpiRAy.c9d8b991-99cf-499b-9af5-546f4d01a8fd", debug: true, contractAddresses: [contractAddr]}
     );
-
-    const biconomy = new Biconomy(
-        customProvider, 
-        {
-            apiKey: "kAsdpiRAy.c9d8b991-99cf-499b-9af5-546f4d01a8fd",
-            debug: true,
-            contractAddresses: [contractAddr]
-        }
-    );
-
+    const web3 = new Web3(biconomyWithWeb3);
+    
+    let contract = new web3.eth.Contract(contractAbi, contractAddr);
     let userAddress = process.env.ACCOUNT_ADDRESS;
     let privateKey = privkey;
 
     let txParams = {
-    "from": userAddress,
-    //gasLimit is optional or can be pre estimated for your method call before passing
-    "gasLimit": web3.utils.toHex(300000), 
-    "to": contractAddr,
-    //optional. Note: Native meta transactions would not work if your method call is expecting ether value transfer and has checkes on msg.value
-    "value": "0x0", 
-    //Call your target method. here we are calling setQuote() method of our contract
-    "data": contract.methods.setQuote(newQuote).encodeABI(), 
+        "from": userAddress,
+        //gasLimit is optional or can be pre estimated for your method call before passing
+        "gasLimit": web3.utils.toHex(300000), 
+        "to": contractAddr,
+        //optional. Note: Native meta transactions would not work if your method call is expecting ether value transfer and has checkes on msg.value
+        "value": "0x0", 
+        //Call your target method. here we are calling setQuote() method of our contract
+        "data": contract.methods.setQuote(newQuote).encodeABI(), 
     };
 
+    console.log(">>>mark1")
     const signedTx = await web3.eth.accounts.signTransaction(txParams, `0x${privateKey}`);
+    console.log(">>>mark2")
     const forwardData = await biconomy.getForwardRequestAndMessageToSign(signedTx.rawTransaction);
 
     // const signature = sigUtil.personalSign(new Buffer.from(privateKey, 'hex'), { data: forwardData.personalSignatureFormat });
